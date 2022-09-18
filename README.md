a15a2247ded74f4bd32c6303c72b68e472d4634f
Matt Welke <mattwelke@gmail.com>
2022-09-17 22:00:20 -0400

    upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# ruby-parallel-concurrency-test
+
+An experiment to achieve parallelism in a concurrent workload in Ruby.
+
+Includes a reproduction of behavior I don't yet understand where when a thread is executed in JRuby, and the block of code executed includes a `puts` statement, the string printed does not have a newline.
+
+Uses CRuby 3.1.2 or JRuby 9.3.8.0 (with Java 19 in order to use Java virtual threads).
+
+Usage:
+
+```
+rvm use [3.1.2|jruby-9.3.8.0] && export RUBY_CMD="[ruby|jruby -J--enable-preview]"
+$RUBY_CMD conc_par_via_java_threads.rb <number_of_units_of_work> <concurrency> [R|JP|JV]
+```
+
+R = Ruby threads
+JP = Java platform threads
+JV = Java virtual threads (requires Java 19 to be set as current Java)
+
+Example output with reproduction of behavior described above:
+
+```
+Running with:
+  Factor: 100000000
+  Workload: 2
+  Concurrency: 2
+  Thread type: JV
+
+Acquired semaphore permit to complete unit of work #1.
+Started unit of work #1.Acquired semaphore permit to complete unit of work #2.
+
+Started unit of work #2.
+Finished unit of work #1.
+Released semaphore permit after completing unit of work #1.
+Joined unit of work #1.
+Finished unit of work #2.
+Released semaphore permit after completing unit of work #2.
+Joined unit of work #2.
+
+End.
+```
+
+Note the single line printed that lacks the expected newline:
+
+```
+Acquired semaphore permit to complete unit of work #1.
+Started unit of work #1.Acquired semaphore permit to complete unit of work #2.
+```
---
+++ par_conc.rb
@@ -0,0 +1,90 @@
+require 'concurrent'
+
+# Config:
+
+# The amount of work to perform in each unit of work.
+FACTOR=100000000
+
+# The number of units of work to perform in total.
+WORKLOAD=Integer(ARGV[0])
+
+# The number of units of work to perform concurrently, with as much as possible
+# being performed concurrently also being performed in parallel (ex. 8 in
+# parallel on a 4 core, 8 thread CPU). This will be the number of virtual
+# threads to start simultaneously.
+CONCURRENCY=Integer(ARGV[1])
+
+# The type of thread to use to run each block of code for a unit of work.
+# Valid values:
+#   R (Ruby thread)
+#   JP (Java platform thread)
+#   JV (Java virtual thread)
+THREAD_TYPE=ARGV[2]
+
+puts "Running with:"
+puts "  Factor: #{FACTOR}"
+puts "  Workload: #{WORKLOAD}"
+puts "  Concurrency: #{CONCURRENCY}"
+puts "  Thread type: #{THREAD_TYPE}"
+puts
+
+def work(factor, n)
+  puts "Started unit of work ##{n}."
+  i = 0
+  factor.times { i += 1 }
+  puts "Finished unit of work ##{n}."
+end
+
+startables = []
+
+# Use a semaphore to restrict the number of runnables we allow to be running at
+# simultaneously.
+semaphore = Concurrent::Semaphore.new(CONCURRENCY)
+
+WORKLOAD.times do |i|
+  n = i + 1
+
+  startable = Proc.new do
+    work(FACTOR, n)
+    semaphore.release
+    puts "Released semaphore permit after completing unit of work ##{n}.\n"
+  end
+
+  startables.push({s: startable, n: n})
+end
+
+threads = []
+
+# Centralizing logic for choosing which type of thread will be used to run the
+# block of code for a work unit. This method also starts the thread immediately
+# before returning because Ruby threads must be started immediately and
+# "Ruby thread" is one of the valid configuration values for thread type.
+def get_thread(proc)
+  if THREAD_TYPE == "R"
+    thread = Thread.new &proc
+    return thread
+  end
+  if THREAD_TYPE == "JP"
+    thread = java.lang.Thread.new(proc)
+    thread.start
+    return thread
+  end
+  if THREAD_TYPE == "JV"
+    thread = java.lang.Thread.ofVirtual
+    return thread.start(proc)
+  end
+end
+
+startables.each do |s|
+  semaphore.acquire
+  puts "Acquired semaphore permit to complete unit of work ##{s[:n]}."
+  thread = get_thread(s[:s])
+  threads << {t: thread, n: s[:n]}
+end
+
+threads.each do |t|
+  t[:t].join
+  puts "Joined unit of work ##{t[:n]}."
+end
+
+puts "\nEnd."
